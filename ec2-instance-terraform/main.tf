32f78d31e1d745ae1eab751f02393704def7a822
md-suhag <abdussalamsuhag104@gmail.com>
2026-08-23 18:40:25 +0600

    add EC2 instance configuration

--- ec2-instance-terraform/main.tf
+++ ec2-instance-terraform/main.tf
@@ -1,28 +1,52 @@
 terraform {
 
-	required_version: ">=1.0"
+  required_version = ">=1.0"
 
-	required_providers {
-	 aws ={
-	 source = "hasicrop/aws"
-	 version = "~>6.0"
-	}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
 }
 
 
 provider "aws" {
 
- region = "us-east-1"
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon_linux" {
+
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "al2023-ami-*-x86_64"
+    ]
+
+  }
+
+
+  filter {
+
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
 }
 
 
 resource "aws_instance" "backend_server" {
 
-	ami =  ""
-	instance_type = "t3.micro"
+  // ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-0332d564d76dbd8d6"
+  instance_type = "t3.micro"
 
-	tags = {
-	 Name = "test-backend-server"
-	}
+  tags = {
+    Name = "test-backend-server"
+  }
 }
---
+++ ec2-instance-terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "instance_public_ip" {
+
+  description = "Public IP address of the EC2 instance"
+
+  value = aws_instance.backend_server.public_ip
+
+}
